88db99fe5d99d77b0c89a7f4e06cba9d44ee58f5
Aliaksei Dzemidchuk <Aliaksei_Dzemidchuk@epam.com>
2018-08-09 14:54:58 +0300

    ssh challenge solution 10: cwd => path

--- site/profile/manifests/master.pp
+++ site/profile/manifests/master.pp
@@ -1,8 +1,9 @@
 class profile::master {
   exec { 'docker exec -ti web.puppet.vm bash -c "/opt/puppetlabs/bin/puppet agent -t"':
-	cwd		=> '/bin',
+	path	=> '/bin',
   }
+  
   exec { 'docker exec -ti db.puppet.vm bash -c "/opt/puppetlabs/bin/puppet agent -t"':
-	cwd		=> '/bin',
+	path	=> '/bin',
   }
 }
